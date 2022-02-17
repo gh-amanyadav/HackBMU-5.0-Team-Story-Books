dacc06ff6bd605ebe4abcbaf074f3cff8bf42bdd
unknown <bajiraouttamsinha@gmail.com>
2022-02-17 21:55:02 +0530

    adding MongoDB to express

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+const mongoose = require('mongoose');
 
 
 require('dotenv').config();
@@ -7,10 +8,19 @@ require('dotenv').config();
 const app = express();
 const port = process.env.PORT || 5000;
 
-
 app.use(cors());
 app.use(express.json());
 
+const url = `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASS}@cluster0.axnkt.mongodb.net/assignment?retryWrites=true&w=majority`;
+mongoose.connect(url, {useNewUrlParser: true});
+const connection = mongoose.connection;
+connection.once('open', () => {
+    console.log('MongoDB is conected');
+});
+
+const storyRouter = require('./routes/stories');
+
+app.use('/stories', storyRouter);
 
 app.listen(port, () => {
     console.log(`Server is running on port: ${port}`);
